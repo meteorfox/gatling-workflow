1aabc7e690aae0a296991a371ec05bd94f394d70
Carlos L. Torres <carlos.torres@rackspace.com>
2015-06-19 19:16:20 -0500

    Add ls statement to view files in dir for debugging

--- gatling_flow.groovy
+++ gatling_flow.groovy
@@ -10,7 +10,8 @@ def distributedJobs = [:]
 for (int i = 0; i < 2; i++) {
 	distributedJobs["gatlingLoadClient${i}"] = {
 		node('slave') {
-			sh "loadbalanced_endpoints_workload.sh"
+			sh 'ls'
+			sh "./loadbalanced_endpoints_workload.sh"
 		}
 	}
 }
